503bbe9edecdacd72119cc1378491cec375bea2e
sigfried <rssanjuan2704@gmail.com>
2020-07-27 21:13:38 -0500

    feat: Add error handlers middlewares

--- server.js
+++ server.js
@@ -10,11 +10,20 @@ app.disable('x-powered-by');
 
 app.use(indexRoute);
 
+app.use((req, res) => {
+  res.status(404).end('Route not found');
+});
+
+app.use((err, req, res, next) => {
+  console.log('Stack trace...');
+  console.log(err.stack);
+  res.status(err.code || 500).end(err.message);
+});
+
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
-
+  console.log(`Server listening on port ${port}`);
+});
