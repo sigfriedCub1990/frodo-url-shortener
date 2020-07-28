d8b5c171d6c329d3fcdfd915d5dfa14f9a1f6902
sigfried <rssanjuan2704@gmail.com>
2020-07-27 21:19:19 -0500

    feat: Get stored URL by hash

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
+const mmh3 = require('murmurhash3');
 
-app.post('/', (req, res) => {
+const { getAsync, setAsync } = require('../db');
+
+const validate = require('../middlewares/validate');
+const lookup = require('../utils/lookup');
+
+const DOMAIN = process.env.DOMAIN || 'https://frodo.sigfried.xyz/';
     const { url } = req.body;
-    res.json({ url })
-})
 
-app.get('/:id', (req, res) => {
+app.get('/:id', async (req, res) => {
+  try {
     const id = req.params.id;
-    res.json({
-        id
-    });
-})
+    const maybeUrl = await getAsync(id);
+    if (maybeUrl) {
+      res.redirect(maybeUrl);
+    } else {
+      res.json({ text: 'URL not found' });
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
 
 module.exports = app;
