ed8f3160688b0786a9846db80d057c61fc746482
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-02-11 20:15:27 +0300

    refactor: Extract business logic from route

---
+++ controllers/shortener.controller.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express';
+// @ts-ignore
+import mmh3 from 'murmurhash3';
+
+import lookup from '../utils/lookup';
+import client from '../db';
+import CustomError from '../utils/error';
+
+const DOMAIN = process.env.DOMAIN || 'https://frodo.sigfried.xyz';
+
+type Next = (err: Error | CustomError) => void;
+
+const ShortenerController = {
+  shortenUrl: async (req: Request, res: Response, next: Next) => {
+    try {
+      const { url } = req.body;
+
+      const urlObject = new URL(url);
+      const lookupRes = await lookup(urlObject.host);
+
+      if (lookupRes) {
+        const urlHash = mmh3.murmur32HexSync(url);
+
+        const maybeKeyValue = await client.get(urlHash);
+        if (!maybeKeyValue) {
+          await client.set(urlHash, url);
+        }
+
+        res.json({ url: `${DOMAIN}/${urlHash}` });
+      }
+    } catch (error) {
+      if (error instanceof CustomError) {
+        next(error);
+      } else if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+    }
+  },
+  getShortenedUrl: async (req: Request, res: Response, next: Next) => {
+    try {
+      const id = req.params.id;
+      const maybeUrl = await client.get(id);
+      if (maybeUrl) {
+        res.redirect(maybeUrl);
+      } else {
+        res.json({ text: 'URL not found' });
+      }
+    } catch (error) {
+      if (error instanceof CustomError) {
+        next(error);
+      } else if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+    }
+  },
+};
+
+export default ShortenerController;
--- routes/index.ts
+++ routes/index.ts
@@ -1,61 +1,12 @@
-import express, { Request, Response } from 'express';
-// @ts-ignore
-import mmh3 from 'murmurhash3';
+import express from 'express';
 
-import client from '../db';
 import validate from '../middlewares/validate';
-import lookup from '../utils/lookup';
-import CustomError from '../utils/error';
+import ShortenerController from '../controllers';
 
 const router = express.Router();
 
-const DOMAIN = process.env.DOMAIN || 'https://frodo.sigfried.xyz';
+router.post('/api/v1/shorten', validate, ShortenerController.shortenUrl);
 
-router.post(
-  '/api/v1/shorten',
-  validate,
-  async (req: Request, res: Response, next) => {
-    try {
-      const { url } = req.body;
-
-      const urlObject = new URL(url);
-      const lookupRes = await lookup(urlObject.host);
-
-      console.log(lookupRes);
-
-      if (lookupRes) {
-        const url_hash = mmh3.murmur32HexSync(url);
-
-        const maybeKeyValue = await client.get(url_hash);
-        if (!maybeKeyValue) {
-          await client.set(url_hash, url);
-        }
-
-        res.json({ url: `${DOMAIN}/${url_hash}` });
-      }
-    } catch (err) {
-      console.error(err);
-      next(err);
-    }
-  },
-);
-
-router.get('/api/v1/shorten/:id', async (req: Request, res: Response, next) => {
-  try {
-    const id = req.params.id;
-    const maybeUrl = await client.get(id);
-    if (maybeUrl) {
-      res.redirect(maybeUrl);
-    } else {
-      res.json({ text: 'URL not found' });
-    }
-  } catch (error: unknown) {
-    if (error instanceof CustomError) {
-      next(error);
-    } else if (error instanceof Error) {
-      throw new Error(error.message);
-    }
-  }
-});
+router.get('/api/v1/shorten/:id', ShortenerController.getShortenedUrl);
 
 export default router;
